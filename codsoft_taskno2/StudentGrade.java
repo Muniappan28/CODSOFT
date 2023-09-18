992ca9004d130fa77d46888500c136b7008cc9c1
Muniappan28 <96340800+Muniappan28@users.noreply.github.com>
2023-09-18 18:07:39 +0530

    Create StudentGrade.java

---
+++ codsoft_taskno2/StudentGrade.java
@@ -0,0 +1,92 @@
+
+package student_grade;
+
+import java.util.Scanner;
+
+public class Student_grade {
+
+    
+    public static void main(String[] args) {
+      
+        Scanner s=new Scanner(System.in);
+        
+        System.out.print("Enter Student Name: ");
+        String student_Name=s.nextLine();
+        
+        System.out.print("Enter total Number of Subject mark to be added: "); 
+        int total_number_subject=s.nextInt();
+        int marks[]=new int[total_number_subject];
+        
+        
+        for(int i=0;i<marks.length;i++){
+        
+            System.out.print("Enter subject "+(i+1)+" mark ");
+            marks[i]=s.nextInt();
+        
+        }
+        
+        
+        int total_marks=0;
+        float avg_marks=0;
+       
+        for(int i=0;i<marks.length;i++)
+        {
+        
+            total_marks=total_marks+marks[i];
+        
+        }
+        
+        avg_marks=total_marks/marks.length;   
+        System.out.println("Total Marks "+total_marks);
+        System.out.println("Average Marks "+avg_marks);
+        
+        if(avg_marks>=90)
+        {
+        
+            System.out.println("O grade");
+        
+        }
+        else if(avg_marks>=80)
+        {
+        
+            System.out.println("A+ grade");
+        
+        }
+        else if(avg_marks>=70)
+        {
+        
+            System.out.println("A grade");
+        
+        }
+        else if(avg_marks>=60)
+        {
+        
+            System.out.println("B+ grade");
+        
+        }
+        else if(avg_marks>=50)
+        {
+        
+            System.out.println("B grade");
+        
+        }
+        else if(avg_marks>=40)
+        {
+        
+            System.out.println("C+ grade");
+        
+        }
+        else if(avg_marks>=30)
+        {
+        
+            System.out.println("C grade");
+        
+        }
+        else
+        {
+        
+            System.out.println("Fail");
+        
+        }
+    }
+}
